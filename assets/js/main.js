21ac3fe0832ed7547d9031442e5b4324620cc948
wwd <panda_wd@163.com>
2018-05-28 14:53:43 +0800

    模板

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,24 @@
 /*
-	Overflow by HTML5 UP
+	Striped by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 (function($) {
 
-	var settings = {
-
-		// Full screen header?
-			fullScreenHeader: true,
-
-		// Parallax background effect?
-			parallax: true,
-
-		// Parallax factor (lower = more intense, higher = less intense).
-			parallaxFactor: 10
-
-	};
-
 	skel.breakpoints({
-		wide: '(max-width: 1680px)',
-		normal: '(max-width: 1080px)',
-		narrow: '(max-width: 840px)',
+		desktop: '(min-width: 737px)',
+		wide: '(min-width: 1201px)',
+		narrow: '(min-width: 737px) and (max-width: 1200px)',
+		narrower: '(min-width: 737px) and (max-width: 1000px)',
 		mobile: '(max-width: 736px)'
 	});
 
 	$(function() {
 
 		var	$window = $(window),
-			$body = $('body');
-
-		if (skel.vars.mobile) {
-
-			settings.parallax = false;
-			$body.addClass('is-scroll');
-
-		}
+			$body = $('body'),
+			$document = $(document);
 
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
@@ -44,10 +26,6 @@
 			$window.on('load', function() {
 				$body.removeClass('is-loading');
 			});
-
-		// CSS polyfills (IE<9).
-			if (skel.vars.IEVersion < 9)
-				$(':last-child').addClass('last-child');
 
 		// Fix: Placeholder polyfill.
 			$('form').placeholder();
@@ -60,102 +38,49 @@
 				);
 			});
 
-		// Scrolly links.
-			$('.scrolly-middle').scrolly({
-				speed: 1000,
-				anchor: 'middle'
-			});
+		// Off-Canvas Sidebar.
 
-			$('.scrolly').scrolly({
-				speed: 1000,
-				offset: function() { return (skel.breakpoint('mobile').active ? 70 : 190); }
-			});
-
-		// Full screen header.
-			if (settings.fullScreenHeader) {
-
-				var $header = $('#header');
-
-				if ($header.length > 0) {
-
-					var $header_header = $header.find('header');
-
-					$window
-						.on('resize.overflow_fsh', function() {
-
-							if (skel.breakpoint('mobile').active)
-								$header.css('padding', '');
-							else {
-
-								var p = Math.max(192, ($window.height() - $header_header.outerHeight()) / 2);
-								$header.css('padding', p + 'px 0 ' + p + 'px 0');
-
-							}
-
-						})
-						.trigger('resize.overflow_fsh');
-
-					$window.load(function() {
-						$window.trigger('resize.overflow_fsh');
-					});
-
-				}
-
-			}
-
-		// Parallax background.
-
-			// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-				if (skel.vars.browser == 'ie'
-				||	skel.vars.mobile)
-					settings.parallax = false;
-
-			if (settings.parallax) {
-
-				var $dummy = $(), $bg;
+			// Height hack.
+				var $sc = $('#sidebar, #content'), tid;
 
 				$window
-					.on('scroll.overflow_parallax', function() {
-
-						// Adjust background position.
-							$bg.css('background-position', 'center ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
-
+					.on('resize', function() {
+						window.clearTimeout(tid);
+						tid = window.setTimeout(function() {
+							$sc.css('min-height', $document.height());
+						}, 100);
 					})
-					.on('resize.overflow_parallax', function() {
-
-						// If we're in a situation where we need to temporarily disable parallax, do so.
-							if (!skel.breakpoint('wide').active
-							||	skel.breakpoint('narrow').active) {
-
-								$body.css('background-position', '');
-								$bg = $dummy;
-
-							}
-
-						// Otherwise, continue as normal.
-							else
-								$bg = $body;
-
-						// Trigger scroll handler.
-							$window.triggerHandler('scroll.overflow_parallax');
-
+					.on('load', function() {
+						$window.trigger('resize');
 					})
-					.trigger('resize.overflow_parallax');
+					.trigger('resize');
 
-			}
+			// Title Bar.
+				$(
+					'<div id="titleBar">' +
+						'<a href="#sidebar" class="toggle"></a>' +
+						'<span class="title">' + $('#logo').html() + '</span>' +
+					'</div>'
+				)
+					.appendTo($body);
 
-		// Poptrox.
-			$('.gallery').poptrox({
-				useBodyOverflow: false,
-				usePopupEasyClose: false,
-				overlayColor: '#0a1919',
-				overlayOpacity: (skel.vars.IEVersion < 9 ? 0 : 0.75),
-				usePopupDefaultStyling: false,
-				usePopupCaption: true,
-				popupLoaderText: '',
-				windowMargin: 10,
-				usePopupNav: true
-			});
+			// Sidebar
+				$('#sidebar')
+					.panel({
+						delay: 500,
+						hideOnClick: true,
+						hideOnSwipe: true,
+						resetScroll: true,
+						resetForms: true,
+						side: 'left',
+						target: $body,
+						visibleClass: 'sidebar-visible'
+					});
+
+			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+					$('#titleBar, #sidebar, #main')
+						.css('transition', 'none');
 
 	});
 
